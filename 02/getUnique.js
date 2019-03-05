4e5cfd822825e12204d4ebe082e371bb67a1370c
GKustov <35932004+GKustov@users.noreply.github.com>
2019-03-05 23:25:58 +0700

    Update getUnique.js

--- 02/getUnique.js
+++ 02/getUnique.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 /**
  * Напишите функцию getUnique(input), возвращающую новый массив,
  * состоящий из уникальных значений массива input. Результирующий
@@ -12,4 +13,11 @@
  * @return {Set<any>} массив уникальных значений, отсортированный по возрастанию
  */
 export function getUnique(input) {
+  const map = new Map();
+
+  input.map(val => { map.set(val, 1); });
+  const result = Array.from(map.keys())
+    .sort((a, b) => a - b);
+
+  return result;
 }
