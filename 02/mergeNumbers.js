175c400ff980a0d15393439da5e296d573aa7f08
GKustov <35932004+GKustov@users.noreply.github.com>
2019-03-05 23:29:54 +0700

    Update mergeNumbers.js

--- 02/mergeNumbers.js
+++ 02/mergeNumbers.js
@@ -12,4 +12,13 @@
  * @param number
  */
 export function mergeNumbers(number) {
+  const str = String(number);
+
+  if (str.length > 1) {
+    const arr = str.split('');
+    const newnums = String(Number.parseInt(arr.shift(), 10) + Number.parseInt(arr.shift(), 10)).split('');
+
+    return mergeNumbers(Number.parseInt(newnums.concat(arr).join(''), 10));
+  }
+  return number;
 }
